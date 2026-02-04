091922ce2e1517db21f30248d1f06cff31317486
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-04 22:58:22 +0530

    today learning

--- MaxDiff.cpp
+++ MaxDiff.cpp
@@ -1,18 +1,36 @@
-int maxDiff(vector<int> &arr) {
-    // Your code here
-       int n = arr.size();
-       if (n < 2) return -1;
+// int maxDiff(vector<int> &arr) {
+//     // Your code here
+//        int n = arr.size();
+//        if (n < 2) return -1;
 
-    int minSoFar = INT_MAX;
-    int maxDiff = -1;
+//     int minSoFar = INT_MAX;
+//     int maxDiff = -1;
 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > minSoFar) {
-            maxDiff = max(maxDiff, arr[i] - minSoFar);
-        } else {
-            minSoFar = arr[i];
-        }
-    }
-    return maxDiff;
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] > minSoFar) {
+//             maxDiff = max(maxDiff, arr[i] - minSoFar);
+//         } else {
+//             minSoFar = arr[i];
+//         }
+//     }
+//     return maxDiff;
 
-}
+// }
+// int maxDiff(vector<int> &arr) {
+//     // Your code here
+//        int n = arr.size();
+//        if (n < 2) return -1;
+
+//     int minSoFar = INT_MAX;
+//     int maxDiff = -1;
+
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] > minSoFar) {
+//             maxDiff = max(maxDiff, arr[i] - minSoFar);
+//         } else {
+//             minSoFar = arr[i];
+//         }
+//     }
+//     return maxDiff;
+
+// }
