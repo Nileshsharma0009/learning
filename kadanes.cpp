9713199d537ef39f347cafe9bdd16b134a8a1a20
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-08 21:51:12 +0530

    ee98038

--- kadanes.cpp
+++ kadanes.cpp
@@ -15,6 +15,23 @@
 //         }
 //         return maxi ;
 //     } 
+// int maxSubarraySum(vector<int> &arr) {
+//         // Code here
+//         long long  currSum= 0 ;
+//         long long maxi=INT_MIN; 
+//         int n = arr.size() ;
+//         for(int i = 0 ; i< n ; i ++){
+//             currSum+=arr[i] ;
+            
+//             maxi = max(currSum , maxi) ;
+            
+//             if(currSum<0 ){
+//                 currSum = 0 ;
+//             }
+            
+//         }
+//         return maxi ;
+//     } 
 
 
 // 6969igiu8956tiugvmnr8375w4546906oiutghhi6w78987oyuigy9ouytghr
