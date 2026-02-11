0033febb1302b5687a9f6df037b0d430f54489d0
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-11 16:32:39 +0530

    solved

--- string .cpp
+++ string .cpp
@@ -21,3 +21,36 @@ vector<int> buildLPS(string &pat) {
     }
     return lps;
 }
+
+vector<int> search(string &pat, string &txt) {
+    vector<int> ans;
+    int n = txt.size();
+    int m = pat.size();
+
+    if (m > n) return ans; // safety check
+
+    vector<int> lps = buildLPS(pat);
+
+    int i = 0; // text pointer
+    int j = 0; // pattern pointer
+
+    while (i < n) {
+        if (txt[i] == pat[j]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            ans.push_back(i - j); // start index
+            j = lps[j - 1];       // continue for overlap
+        }
+        else if (i < n && txt[i] != pat[j]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return ans;
+}
