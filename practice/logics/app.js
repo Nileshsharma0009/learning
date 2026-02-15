63bad9239056875eb2990bb991b59e689378723d
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-15 15:32:34 +0530

    392

---
+++ practice/logics/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors"
+
+const app = express() ;
+
+app.use(
+    cors([
+        origin:[
+            "url 2" ,
+            "url 3",
+        ],
+
+        credentials:  true ,
+    ])
+);
+
+app.use(express.json()) ;
+app.use(cookieParser()) ;
+
+app.get("/api/health" , (req , res) => {
+    res.json({ok: true});
+});
+
+
+app.use("api/auth",authRoutes);
