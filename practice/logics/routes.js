807c9e9fbed60da1d0a040875591373a98a2b2f5
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-15 15:47:46 +0530

    update files

--- practice/logics/routes.js
+++ practice/logics/routes.js
@@ -1,2 +1,9 @@
 
-export cons
+export const  resgiter =   (req , res) =>{
+     const {name , email, password } = req.body ;
+
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+
+}
