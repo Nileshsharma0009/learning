b9f2f473777b7988c69de9f978f64110afd58164
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-16 21:13:05 +0530

    hm,

--- practice/logics/routes.js
+++ practice/logics/routes.js
@@ -22,3 +22,51 @@ export const  signup =   (req , res) =>{
      }
 
 }
+
+export const  resgiter =   (req , res) =>{
+     const {name , email, password } = req.body ;
+
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+
+}
+
+export const  signup =   (req , res) =>{
+     const {name , email, password } = req.body ;
+
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+
+}
+
+export const  resgiter =   (req , res) =>{
+     const {name , email, password } = req.body ;
+
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+
+}
+
+export const  signup =   (req , res) =>{
+     const {name , email, password } = req.body ;
+
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+     if(!name ||  !email || ! password){
+        return res.status(400).json({error : "all fieldsa are required"});
+     }
+
+}
