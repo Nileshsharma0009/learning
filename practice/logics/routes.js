8e091083a02d3255a40f528e3188e58b7f245582
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-18 09:23:13 +0530

    regisdter

--- practice/logics/routes.js
+++ practice/logics/routes.js
@@ -1,72 +1,31 @@
 
-export const  resgiter =   (req , res) =>{
+export const  resgiter =  async (req , res) =>{
      const {name , email, password } = req.body ;
 
      if(!name ||  !email || ! password){
         return res.status(400).json({error : "all fieldsa are required"});
      }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
+  
+     const emailExist =  await  User.findOne({email})
+     if(emailExist){
+      return  res.status(400).json({message :"email already exists"})
      }
+
+     const hashPassword = await bcrypt.hash(password ,10)  ;
+
+     const  user = await User.create({
+      name , email , password :hashPassword,
+
+     });
+
+     res.status(201).json({message : "registration sucessful" ,
+      user :{
+          id=user._id,
+          name:user.name ,
+          email:user.email,
+      }
+     })
 
 }
 
-export const  signup =   (req , res) =>{
-     const {name , email, password } = req.body ;
 
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-
-}
-
-export const  resgiter =   (req , res) =>{
-     const {name , email, password } = req.body ;
-
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-
-}
-
-export const  signup =   (req , res) =>{
-     const {name , email, password } = req.body ;
-
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-
-}
-
-export const  resgiter =   (req , res) =>{
-     const {name , email, password } = req.body ;
-
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-
-}
-
-export const  signup =   (req , res) =>{
-     const {name , email, password } = req.body ;
-
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-     if(!name ||  !email || ! password){
-        return res.status(400).json({error : "all fieldsa are required"});
-     }
-
-}
