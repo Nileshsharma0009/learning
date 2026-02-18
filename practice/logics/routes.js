c7b078a33571f5a16ca0494be556497c1867ba3b
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-18 11:54:49 +0530

    34297u0wq

--- practice/logics/routes.js
+++ practice/logics/routes.js
@@ -29,3 +29,60 @@ export const  resgiter =  async (req , res) =>{
 }
 
 
+
+
+
+export const  signup = async (req , res) => {
+
+   try{
+      const {email , password} = req.body ;
+
+      if(!email || !password){
+         return res.status(400).json({error : "all fields are required"});
+      }
+
+
+
+      const existUser = await User.findOne({email})
+
+      if(existUser){
+         return res.status(400).json({error : "invalid credentials"});
+      }
+      
+
+      const hashPassword = await bcrypt.hash(password , 10) ;
+
+          const user = await  User.create({
+               email , password : hashPassword
+          })
+
+          const token = jwt.sign({id : user._id} , process.env.JWT_SECRET , {expiresIn : "1d"})
+
+          res.status(200).json({message : "login successful" , token})
+
+
+          res.cookie("token" ,toke ,{
+               httpOnly: true ,
+               secure: process.env.NODE_ENV === "production" ,
+               sameSite: "strict" ,
+               maxAge : 24 * 60 * 60 * 1000 ,
+          })
+          res.status(200).json({message : "login successful" , token})
+
+              res.status(201).json({
+      user: { id: user._id, name: user.name, email: user.email, createdAt: user.createdAt, updatedAt: user.updatedAt },
+
+    });
+        
+
+              
+
+
+          
+   }catch(errr){
+    console.error(errr);
+    res.status(500).json({error : "server error"})
+   }
+}
+
+
