86236ffeb512db133ba65d92de6d2ad9f64d631e
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-23 20:40:18 +0530

    89342

---
+++ c++/hello.cpp
@@ -0,0 +1,20 @@
+vector<int> productExceptSelf(vector<int>& nums) {
+    int s = nums.size(); 
+    vector<int> answer(s, 1); 
+    int prefix = 1; 
+
+  
+    for (int i = 0; i < s; i++) {
+        answer[i] = prefix; 
+        prefix *= nums[i]; 
+    }
+
+
+    int suffix = 1; 
+    for (int i = s - 1; i >= 0; i--) {
+        answer[i] *= suffix; 
+        suffix *= nums[i]; 
+    }
+
+    return answer; 
+}
