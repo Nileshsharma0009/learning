725856db3233541450e4c34674f037af93c53a0e
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-02-24 18:03:06 +0530

    89572

--- c++/hello.cpp
+++ c++/hello.cpp
@@ -18,67 +18,28 @@ vector<int> productExceptSelf(vector<int>& nums) {
 
     return answer; 
 }
-}
-vector<int> productExceptSelf(vector<int>& nums) {
-    int s = nums.size(); 
-    vector<int> answer(s, 1); 
-    int prefix = 1; 
 
-  
-    for (int i = 0; i < s; i++) {
-        answer[i] = prefix; 
-        prefix *= nums[i]; 
+
+class Solution {
+public:
+    int find(vector<int>& arr, int index, int target) {
+        // Base Case 1: If target becomes 0, we found 1 valid subset
+        if (target == 0) return 1;
+
+        // Base Case 2: If we've gone through all elements or target is impossible
+        if (index >= arr.size() || target < 0) return 0;
+
+        // Choice 1: Include the current element (subtract from target)
+        int pick = find(arr, index + 1, target - arr[index]);
+
+        // Choice 2: Exclude the current element (target remains same)
+        int skip = find(arr, index + 1, target);
+
+        // Return the total count from both branches
+        return pick + skip;
     }
 
-
-    int suffix = 1; 
-    for (int i = s - 1; i >= 0; i--) {
-        answer[i] *= suffix; 
-        suffix *= nums[i]; 
+    int countSubsetsWithSum(vector<int>& arr, int target) {
+        return find(arr, 0, target);
     }
-
-    return answer; 
-}
-}
-vector<int> productExceptSelf(vector<int>& nums) {
-    int s = nums.size(); 
-    vector<int> answer(s, 1); 
-    int prefix = 1; 
-
-  
-    for (int i = 0; i < s; i++) {
-        answer[i] = prefix; 
-        prefix *= nums[i]; 
-    }
-
-
-    int suffix = 1; 
-    for (int i = s - 1; i >= 0; i--) {
-        answer[i] *= suffix; 
-        suffix *= nums[i]; 
-    }
-
-    return answer; 
-}
-}
-vector<int> productExceptSelf(vector<int>& nums) {
-    int s = nums.size(); 
-    vector<int> answer(s, 1); 
-    int prefix = 1; 
-
-  
-    for (int i = 0; i < s; i++) {
-        answer[i] = prefix; 
-        prefix *= nums[i]; 
-    }
-
-
-    int suffix = 1; 
-    for (int i = s - 1; i >= 0; i--) {
-        answer[i] *= suffix; 
-        suffix *= nums[i]; 
-    }
-
-    return answer; 
-}
-}
+};
