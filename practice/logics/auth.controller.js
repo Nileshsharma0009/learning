d3a542d60cc6bd315773702ee0c600cbea1512b3
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-07 21:06:36 +0530

    457

--- practice/logics/auth.controller.js
+++ practice/logics/auth.controller.js
@@ -69,4 +69,14 @@ const signup =  (req,res) => {
 }
 
 
-const Login =  async  ()
+const Login =  async  (req , res) =>{
+    try {
+        const {email , password} = req.body ;
+
+        if(!email || !password){
+            return res.status(400).json({error : "all fields are required"});
+        }
+
+        const user
+    }
+}
