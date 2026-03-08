021f687ef5d8c44906ef92501b18198e7ec8405c
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-08 16:36:01 +0530

    546

---
+++ practice/logics/question controller.js
@@ -0,0 +1,13 @@
+
+
+const getQuestion = async(req , res) => {
+    try {
+     
+        const {mockIUD , sectionn ,subjetv} = req.param ;
+
+        const  questoion = await Question.find
+    }catch(error){
+
+    }
+
+}
