173033c7e1605f48889eb9d37af09fbae7aa53d2
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-08 16:55:44 +0530

    0985234

--- practice/logics/question controller.js
+++ practice/logics/question controller.js
@@ -5,9 +5,14 @@ const getQuestion = async(req , res) => {
      
         const {mockIUD , sectionn ,subjetv} = req.param ;
 
-        const  questoion = await Question.find
-    }catch(error){
+        const  questoion = await Question.find({
+            mockIUD , sectionn ,subjetv
+        } ,{})
 
+        res.status(200).json(question);
+
+    }catch(error){
+      res,status(500).json({message:"failed to fetch"})
     }
 
 }
