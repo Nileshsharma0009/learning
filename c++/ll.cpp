daed6df8a6d27fb8ca331a5ba65586bd6f6f7152
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-09 20:12:12 +0530

    658

--- c++/ll.cpp
+++ c++/ll.cpp
@@ -1,16 +1,12 @@
-vector<int > Build(string & pat){
-	int n = pat.size();
-   vector<int> lps(n ,0);
-	int prefix=0 ;
-	int suffix = 1;
-
-	while(suffix < n){
-		if(pat[prefix]==pat[suffix]){
-			prefix++;
-			lps[suffix]=prefix ;
-			suffix;
+vector<vector<int>> transpose(vector<vector<int>>& mat) {
+    int   row = mat.size() , col = mat[0].size() ;
+  
+	for(int i = 0 ;i< row ;i++){
+		for(int j = i+ 1;j < col; j++){
+			swap(mat[i][j] ,mat[j][i]) ;
+			
+			
 		}
 	}
-
+	return mat ;
 }
-
