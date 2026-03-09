a13dc6cc52322ca5e3f4e1a1e34aab4ba0d813d7
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-09 17:40:08 +0530

    456567

--- c++/ll.cpp
+++ c++/ll.cpp
@@ -14,38 +14,3 @@ vector<int > Build(string & pat){
 
 }
 
-
-vector<int> search(string &pat, string &txt) {
-    // Your code here
-    int n = pat.size();
-	int m = txt.size();
-    vector<int> ans ;
-	vector<int> LPS = Build(pat) ;
-
-	if( n > m ){
-		return;
-	}
-
-	int text = 0 , pattern = 0 ;
-
-	while(text < m){
-		if(pat[pattern]==txt[text]){
-			text++, pattern++;
-		}
-
-		if(pattern == n){
-			ans.push_back(text - pattern);
-
-			pattern = lps[pattern -1];
-		}else if(text < n && txt[text] != pat[pattern]){
-			if(pattern!= 0){
-				pattern - lps[pattern -1] ;
-			}else{
-				text++ ;
-			}
-		}
-	}return ans ;
-
-
-		
-}
