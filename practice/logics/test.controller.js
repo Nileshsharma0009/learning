12a016996f575f91e6bd6576c3b17f1df3a1acb1
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-09 17:37:48 +0530

    766

--- practice/logics/test.controller.js
+++ practice/logics/test.controller.js
@@ -22,3 +22,27 @@ const submitTest = async (req ,res) => {
 
     }
 }
+
+const submitTest = async (req ,res) => {
+    try{
+       const {mockId , answers} = req.body ;
+         const userId = req.user.id ;
+
+         let result = await Result.fimdOne({userId,mockId}) ;
+
+         if(result){
+            return res.status(409).json({
+                message :"your have alraedy submitted this test" ,
+                resultid:result,
+
+            })
+         }
+
+         const finalanswer =  result ?{...result.answers ,...answers} : answers ;
+
+         const 
+
+    }catch(err){
+
+    }
+}
