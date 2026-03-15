16bbb9268baedfb11424e58b1f12d1105f2c71b9
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-03-15 14:31:16 +0530

    397587

--- c++/ll.cpp
+++ c++/ll.cpp
@@ -9,49 +9,59 @@
  * };
  */
 
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
-{
-    ListNode dummy(0);
-    ListNode* tail = &dummy;
+// merge two sorted lists
+ListNode* sortedMerge(ListNode* head1, ListNode* head2) {
 
-    while(l1 && l2)
-    {
-        if(l1->val <= l2->val)
-        {
-            tail->next = l1;
-            l1 = l1->next;
+    if(!head1) return head2;
+    if(!head2) return head1;
+
+    ListNode* dummy = new ListNode(-1);
+    ListNode* tail = dummy;
+
+    while(head1 != NULL && head2 != NULL) {
+
+        if(head1->val <= head2->val) {
+            tail->next = head1;
+            head1 = head1->next;
         }
-        else
-        {
-            tail->next = l2;
-            l2 = l2->next;
+        else {
+            tail->next = head2;
+            head2 = head2->next;
         }
 
         tail = tail->next;
     }
 
-    if(l1) tail->next = l1;
-    else tail->next = l2;
+    if(head1 != NULL)
+        tail->next = head1;
+    else
+        tail->next = head2;
 
-    return dummy.next;
+    return dummy->next;
 }
 
-ListNode* solve(vector<ListNode*>& lists, int left, int right)
-{
+
+// divide and merge
+ListNode* solve(vector<ListNode*>& lists, int left, int right) {
+
     if(left > right) return NULL;
-    if(left == right) return lists[left];
+
+    if(left == right)
+        return lists[left];
 
     int mid = (left + right) / 2;
 
     ListNode* l1 = solve(lists, left, mid);
     ListNode* l2 = solve(lists, mid + 1, right);
 
-    return mergeTwoLists(l1, l2);
+    return sortedMerge(l1, l2);
 }
 
+
 ListNode* mergeKLists(vector<ListNode*>& lists) {
-    
-    if(lists.size() == 0) return NULL;
+
+    if(lists.size() == 0)
+        return NULL;
 
     return solve(lists, 0, lists.size() - 1);
 }
